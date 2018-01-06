306265d57981e605bcc05d4287154f8803ac2b83
suhastj <suhastj@gmail.com>
2018-01-06 18:14:39 -0500

    Add files via upload

---
+++ HackerRank/src/com/tj/hackerrank/algo/strings/SuperReducedString.java
@@ -0,0 +1,25 @@
+package com.tj.hackerrank.algo.strings;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class SuperReducedString {
+
+	static String super_reduced_string(String s){
+        // Complete this function
+        boolean finalString
+        for(){
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String result = super_reduced_string(s);
+        System.out.println(result);
+    }
+}
